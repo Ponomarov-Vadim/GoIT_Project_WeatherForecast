2b8eab8cd1b5c0bd2170fdb7baa4e71d93beff11
Anastasiia <nastyona221985@gmail.com>
2020-04-27 19:37:17 +0300

    changed files location

--- src/index.js
+++ src/index.js
@@ -6,16 +6,8 @@ import './javascript/responseParser';
 import './javascript/mainPageWeatherInfo';
 import './javascript/main';
 
-import phrases from './phrases';
-import randomNumberSelection from './randomNumberSelection.js';
-import markup from './template/phrasesTemplate.hbs';
-import dateTimeLoader from "./date-time-info/loadDate";
 
-const markupForPhrases = function (markup) {
-  const randomPhrases = phrases[randomNumberSelection(0, phrases.length - 1)];
-  const markupPhrases = markup(randomPhrases);
-  document
-    .querySelector('.quote-wrap')
-    .insertAdjacentHTML('beforeend', markupPhrases);
-};
-markupForPhrases(markup);
+import './phrases/phrases-markup';
+import "./date-time-info/loadDate";
+
+
