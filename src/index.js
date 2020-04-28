3d0e0b44da4c6eaa786ce3db079c5b9160df516e
Dmitriy Pismenniy <dima30655@gmail.com>
2020-04-28 20:34:53 +0300

    fixed 5 days info

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './sass/styles.scss';
-import './js/base/block-hidden'
+import './js/base/block-hidden';
 
 import './js/input/add-city';
 import './js/input/navigation';
 import './js/base/main';
 import './js/quote/change_quote';
 import './js/base/five_days_weather';
-import './date-time-info/loadDate'
+import './date-time-info/loadDate';
