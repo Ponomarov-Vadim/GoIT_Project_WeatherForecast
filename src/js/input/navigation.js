a8879c2a8c68316868b172ce6f8885c61cf6e8cc
Dmitriy Pismenniy <dima30655@gmail.com>
2020-04-29 18:29:41 +0300

    change in input js

--- src/js/input/navigation.js
+++ src/js/input/navigation.js
@@ -1,23 +1,29 @@
 import pullRequest from '../base/main';
 
-function getCurrentPosition(e) {
+function getCurrentPosition ( e ) {
   const options = {
     timeout: 10000,
   };
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options);
-  });
+  return new Promise( ( resolve, reject ) => {
+    navigator.geolocation.getCurrentPosition( resolve, reject, options );
+  } );
 }
 
-function getCoords(e) {
+function getCoords ( e ) {
   getCurrentPosition()
-    .then(location => {
-      const lat = location.coords.latitude.toFixed(4);
-      const lon = location.coords.longitude.toFixed(4);
-      pullRequest('', lat, lon);
-    })
-    .catch(error => console.log(error));
+    .then( location => {
+      const lat = location.coords.latitude.toFixed( 4 );
+      const lon = location.coords.longitude.toFixed( 4 );
+      pullRequest( '', lat, lon );
+      setTimeout( () => {
+        const inputSearchCity = document.querySelector( '.search-wrap__form-input' );
+        const cityName = document.querySelector( `.main-city-weather__city-name` );
+        console.dir( inputSearchCity );
+        inputSearchCity.value = cityName.textContent;
+      }, 400 );
+    } )
+    .catch( error => console.log( error ) );
 }
 
-const btnLocation = document.querySelector('#location');
-btnLocation.addEventListener('click', getCoords);
+const btnLocation = document.querySelector( '#location' );
+btnLocation.addEventListener( 'click', getCoords );
